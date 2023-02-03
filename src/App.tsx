8eb258e4ef9da95197014ead20c18199b718e986
Eduard <mr.ed-c@yandex.ru>
2023-02-03 18:09:25 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './pages/Home';
+import './styles/index.sass'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <div className='block'>
+        Home
+      </div>
+      <div></div>
+    </>
+  )
+}
+
+export default Home
