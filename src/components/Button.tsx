735baed702b9d42823bdcce81d242b38193b7df8
Balakin Ivan <ivanbal1821@yandex.ru>
2023-02-05 13:10:50 +0300

    1

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import racon from '../img/logo.svg'
+
+function Button() {
+  return (
+    <>
+    <button></button>
+    </>
+  )
+}
+
+export default Button
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import Button from '../components/Button'
+import logo from '../img/logo.svg'
 
 function Home() {
   return (
     <>
-      <div className='block'>
-        Home
-      </div>
-      <div></div>
+      <header>
+        <div className="top">
+          <p className="header__text">Мы открылись! Теперь доступна доставка и самовывоз</p>
+          <button className="header__btn">Сделать заказ</button>
+        </div>
+        <div className="bottom">
+          <div className="header__logo"><img src={logo}/></div>
+          <nav className="header__nav">Глав</nav>
+          <Button></Button>
+        </div>        
+      </header>
+
+      <body>
+        
+      </body>
+
+      <footer>
+
+      </footer>
     </>
   )
 }
