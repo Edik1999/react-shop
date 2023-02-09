34294e12641eea3c40c6e99250bfd72ab755b6e2
Balakin Ivan <ivanbal1821@yandex.ru>
2023-02-09 23:43:02 +0300

    few changes

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -10,7 +10,7 @@ function Contacts() {
           <h2 className="section__title contacts__title text-color">Call our store and takeaway when it suits you best.</h2>
           <p className="contacts__text">Leo vel orci porta non pulvinar neque laoreet. Quis risus sed vulputate odio ut enim blandit volutpat maecenas. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Semper auctor neque vitae tempus quam pellentesque nec.</p>
           <p className="contacts__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <a className="contacts__link" href="tel:+79876543210"><Button modificator="page-btn" text="+7 (987) 654 32-10" onClick=""></Button></a>
+          <a className="contacts__link btn page-btn" href="tel:+79876543210">+7 (987) 654 32-10</a>
         </div>
         <div className="contacts__right">
           <img className="contacts__img" src={contactsdecorate} />
