7bc1e2bd76fba92738fa24d3c59db9961f5f844f
Eduard Chernobrovkin <eduardchernobrovkin@gmail.com>
2023-03-06 13:21:04 +0300

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,29 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import {BrowserRouter} from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCkGk65zNBdpagu52sOa887fjNYbdEeDgk",
+    authDomain: "react-shoppp.firebaseapp.com",
+    projectId: "react-shoppp",
+    storageBucket: "react-shoppp.appspot.com",
+    messagingSenderId: "1059732689195",
+    appId: "1:1059732689195:web:5707f4d9fe9130ad08b56e",
+    measurementId: "G-2ZP5WE5XGY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,9 +38,9 @@ root.render(
     <BrowserRouter>
         <Auth0Provider
             domain="react-shop-test-auth.eu.auth0.com"
-            clientId="JcylG0svGWHEhg7O4cFNCweZPrVhlWnD"
+            clientId="ddya3bPc6X82PQcIp7M89aDIISgA2c8t"
             authorizationParams={{
-                redirect_uri: "http://localhost:3000/menu"
+                redirect_uri: window.location.origin
             }}
         >
             <App />
